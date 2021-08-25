351e0b9c8a5ffb29ef7c3aba63a87654b607495a
azzam008 <87006222+azzam008@users.noreply.github.com>
2021-08-26 01:38:45 +0300

    رياضيات +
    كود رياضيات للزائد +

---
+++ Ryadhyat.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a:"))
+b=int(input("Enter a:"))
+ca=int(input("Enter a:"))
+n=int(input("Enter a:"))
+print("total=",a+b+ca+n)
+print("--------------------")
+print("Azzam Salim Al-harbi")
+
